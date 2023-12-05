c334c9b4be31eeea86a9d2a1b931a1462ce9ed54
Tes <tes_7583@hotmail.com>
2023-12-05 14:48:30 +0100

    Lært datatyper manger bool

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("2Z er den bedste klasse nogensinde");
+
+/*Datatypes: int, float, string, bool
+int are whole numbers (-5, 6 , 57894132)
+float decimal numbers
+string is Text in ""
+*/
+
+// int x; //Declared x
+// x = 4; //Instansiated x
+
+// int y = 5, z = 6; //Declared and instansiated y and z
+
+// Console.WriteLine("x is " + x + " y is " + y + " z is " + z);
+
+// float d = 5.5f;
+
+// Console.WriteLine(d + x - z / y * d % x);
+
+// int e = Convert.ToInt16(d);
+
+// Console.WriteLine("float to int " + e);
+
+
+string str = "I am a string" + "\nAnd so am I";
+
+Console.WriteLine(str);
