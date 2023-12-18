ff73589c0ccec5d078c1a77ad669e4672b91f19c
Tes <tes_7583@hotmail.com>
2023-12-18 14:42:59 +0100

    new switch tricks

--- Examples/SwitchCase.cs
+++ Examples/SwitchCase.cs
@@ -1,43 +1,43 @@
-using System.Collections;
-using System.Security.Cryptography;
+// using System.Collections;
+// using System.Security.Cryptography;
 
-class SwitchCase
-{
+// class SwitchCase
+// {
 
-    static void Main()
-    {
-        int y = 5;
-        switch (y)
-        {
-            case > 4 and 8:
-                Console.WriteLine("y is bigger than 4 and 5");
-                break;
-            case <= 4:
-                Console.WriteLine("y is 5.3");
-                break;
-            case 6:
-            case 7:
-                Console.WriteLine("y is 6 or 7");
-                break;
-            default:
-                Console.WriteLine("a case for y is not defined");
-                break;
-        }
+//     static void Main()
+//     {
+//         int y = 5;
+//         switch (y)
+//         {
+//             case > 4 and 8:
+//                 Console.WriteLine("y is bigger than 4 and 5");
+//                 break;
+//             case <= 4:
+//                 Console.WriteLine("y is 5.3");
+//                 break;
+//             case 6:
+//             case 7:
+//                 Console.WriteLine("y is 6 or 7");
+//                 break;
+//             default:
+//                 Console.WriteLine("a case for y is not defined");
+//                 break;
+//         }
 
-        string name = "emily";
+//         string name = "emily";
 
-        switch (name)
-        {
-            case "John" or "john":
-                Console.WriteLine("John/john");
-                break;
-            case "Emily":
-            case "emily":
-                Console.WriteLine("Emily/emily");
-                break;
-        }
-    }
+//         switch (name)
+//         {
+//             case "John" or "john":
+//                 Console.WriteLine("John/john");
+//                 break;
+//             case "Emily":
+//             case "emily":
+//                 Console.WriteLine("Emily/emily");
+//                 break;
+//         }
+//     }
 
 
 
-}
+// }
