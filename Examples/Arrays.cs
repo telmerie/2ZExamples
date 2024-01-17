cb7013ecd1fcb20589c7768bbda566ea4084b9ff
Tes <tes_7583@hotmail.com>
2024-01-17 12:23:35 +0100

    array

---
+++ Examples/Arrays.cs
@@ -0,0 +1,9 @@
+class Arrays{
+    public static void Main(){
+        string[] names =  ["John", "Jane"];
+
+        Console.WriteLine($"The length of the array names is {names.Length}");
+
+        Console.WriteLine($"The values in name are: {names}");
+    }
+}
