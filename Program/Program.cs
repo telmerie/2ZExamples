f6c78f7575d3a776424a9531604dd5c05a40c5e1
Tes <tes_7583@hotmail.com>
2024-03-27 10:18:54 +0100

    update

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,7 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Print();
+﻿public static class Program {
 
-static void Print(){
-    Console.WriteLine("print");
+    public static void Main(){
+        Console.Write("Input number of Fibonacci Series : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Fibonacci(n);
+
+    }
+
+    public static void Fibonacci(int n){
+        int first = 0; 
+        int second = 1; 
+        int res;
+
+        for( int i = 0; i < n; i++){
+            Console.Write(first + " ");
+            res = first + second;
+            first = second;
+            second = res;
+        }   
+
+    }
+
+    public static void Velcome(string name){
+
+        Console.WriteLine("Have a nice day!");
+
+    }
 }
+
+
